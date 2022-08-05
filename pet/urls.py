06faeea72148f91c17cc5ccd2ca99060e35b93ab
tunv <tunv@newwave.vn>
2022-08-05 22:34:28 +0700

    refactor: add new url

--- pet/urls.py
+++ pet/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, PetListView
+
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('pets/', PetListView.as_view(), name='pet_list'),
 ]
