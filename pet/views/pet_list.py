43aa6cbcf3ca33c1c7e6c11e59a52faf625faf34
tunv <tunv@newwave.vn>
2022-08-05 22:35:08 +0700

    feat: add pet list view

---
+++ pet/views/pet_list.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from ..models.pet import Pet
+
+
+class PetListView(ListView):
+    model = Pet
+    template_name = 'pet_list.html'
