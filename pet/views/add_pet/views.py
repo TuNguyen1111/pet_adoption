a864517740d03ca18400026d5211f73e114af13b
tunv <tunguyen060811@gmail.com>
2022-10-15 12:09:29 +0700

    add  login requried

--- pet/views/add_pet/views.py
+++ pet/views/add_pet/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from ...forms import PetForm
 
-class AddPetView(View):
+
+# TODO: set permission for user who can add new pet
+class AddPetView(LoginRequiredMixin, View):
+    login_url = '/login'
     template_name = 'add_pet.html'
     form = PetForm
 
