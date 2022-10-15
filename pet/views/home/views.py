e2da760a71436d418094b89019672d8e24eaf80c
tunv <tunguyen060811@gmail.com>
2022-10-15 12:08:51 +0700

    refactor code

--- pet/views/home/views.py
+++ pet/views/home/views.py
@@ -1,10 +1,9 @@
-from re import template
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 
 class HomeView(View):
     template_name = 'home.html'
+
     def get(self, request):
         return render(request, self.template_name)
