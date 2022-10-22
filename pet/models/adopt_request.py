c339fa47c8e91abf6132fa0b85fab3516c63d711
tunv <tunguyen060811@gmail.com>
2022-10-22 22:53:06 +0700

    update model

--- pet/models/adopt_request.py
+++ pet/models/adopt_request.py
@@ -1,4 +1,5 @@
 from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.utils import timezone
 
@@ -9,6 +10,11 @@ from .pet import Pet
 class AdoptRequest(models.Model):
     adopt = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     pet = models.ForeignKey(Pet, null=True, on_delete=models.SET_NULL)
+    adopter_first_name = models.CharField(max_length=100, null=True, blank=True)
+    adopter_last_name = models.CharField(max_length=100, null=True, blank=True)
+    adopter_age = models.IntegerField(null=True, blank=True)
+    adopter_address = models.CharField(max_length=100, null=True, blank=True)
+    adopt_reason = models.CharField(max_length=1000, null=True, blank=True)
     accept_for_interviewing = models.BooleanField(default=False)
     request_date = models.DateTimeField(default=timezone.now)
     
