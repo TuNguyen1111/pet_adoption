c50102f97e3ccabeeaea28e6014b775f8a4b5f79
tunv <tunguyen060811@gmail.com>
2022-11-12 11:49:57 +0700

    create new model

--- pet/admin.py
+++ pet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Pet, Animal, PetImages, AdoptRequest
+from .models import User, Pet, Animal, PetImages, AdoptRequest, Adoption
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
@@ -8,3 +8,4 @@ admin.site.register(Pet)
 admin.site.register(Animal)
 admin.site.register(PetImages)
 admin.site.register(AdoptRequest)
+admin.site.register(Adoption)
--- pet/models/pet.py
+++ pet/models/pet.py
@@ -4,7 +4,6 @@ from .animals import Animal
 # Create your models here.
 
 class Pet(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE, null=False, blank=False)
     name = models.CharField(max_length=100)
     age = models.IntegerField()
